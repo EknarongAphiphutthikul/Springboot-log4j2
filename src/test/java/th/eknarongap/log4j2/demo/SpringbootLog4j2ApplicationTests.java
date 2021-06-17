dc9aab8a58121b083c96371282cd007cfa3763e3
EknarongAphiphutthikul <akenarongake@hotmail.com>
2021-06-17 23:06:25 +0700

    init project

---
+++ src/test/java/th/eknarongap/log4j2/demo/SpringbootLog4j2ApplicationTests.java
@@ -0,0 +1,13 @@
+package th.eknarongap.log4j2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLog4j2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
